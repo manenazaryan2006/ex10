c891d094c1f3d42b1741e83839f64bd22aab55b8
manenazaryan2006 <nazaryanmane496@gmail.com>
2024-10-17 23:10:11 +0400

    Create ex10.cpp

---
+++ ex10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int mnacord;
+    int a,b,c,d,e;
+    std::cout<<"nermucel 3 avandneri gumarnery ev 2 dursberman gumarnery";
+    std::cin>>a>>b>>c>>d>>e;
+    std::cout<<"mnacordy "<<mnacord+a+b+c-d-e;
+    
+
+    return 0;
+}
